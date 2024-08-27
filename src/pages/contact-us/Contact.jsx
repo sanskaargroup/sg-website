78a25f9956ffba6a132c2a9ffe2230a82e749507
Tanay Kumar <tanaykmrdev@gmail.com>
2024-08-27 13:53:15 +0530

    minor changes to contact-us page

--- src/pages/contact-us/Contact.jsx
+++ src/pages/contact-us/Contact.jsx
@@ -1,23 +1,26 @@
+import BackgroundImage from "../../image/s7.jpg";
+import img from "../../image/contact.jpg";
+import img2 from "../../image/contactusimg.jpg";
+import "./Contact.css";
+import EnquiryForm from "../enquiry-form/EnquiryForm";
 
-import BackgroundImage from '../../image/s7.jpg'
-import img from '../../image/contact.jpg';
-import img2 from "../../image/contactusimg.jpg"
-import './Contact.css'
-import EnquiryForm from "../enquiry-form/EnquiryForm"
-
-const  Contact = () => {
-  return (
-    <div className="Form">
-      <div className="head" style={{ backgroundImage: `url(${BackgroundImage})` }}>
-        <h1>Services</h1>
-      </div>
-      <div className="image">
-        <img src={img2} alt="" style={{"height": "250px", width: "250px" }} />
-      </div>
-      <p className="para">Get in touch and We&apos;ll get back to you as soon as we can. We look forward to hearing from you!</p>
-      <EnquiryForm />
-    </div>
-  );
+const Contact = () => {
+	return (
+		<div className="Form">
+			<div className="head" style={{backgroundImage: `url(${BackgroundImage})`}}>
+				<h1>Services</h1>
+			</div>
+			<div className="image">
+				<img src={img2} alt="" style={{height: "250px", width: "250px"}} />
+			</div>
+			<p className="para">
+				Have a question or want to book an event? Reach out to us, and we'll get back to you promptly. <br />
+				We're excited to hear from you!
+        
+			</p>
+			<EnquiryForm />
+		</div>
+	);
 };
 
 export default Contact;
