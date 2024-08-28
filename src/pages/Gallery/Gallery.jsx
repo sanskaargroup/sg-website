1e9c5fb961346008f2d376f7a7fb34b39b8cd7d6
Kaushal5033 <kaushalrai8076958922@gmail.com>
2024-08-28 23:40:45 +0530

    gallery done

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,11 +1,120 @@
-import "./Gallery.css"
-import coming from "../../image/coming.gif"
+import React from 'react';
+import "./Gallery.css";
+import sc1 from "../../image/sc1.jpg";
+import sc2 from "../../image/sc2.jpg";
+import sc3 from "../../image/sc3.jpg";
+const events = [
+  {
+    id: 1,
+    title: "Wedding Ceremony",
+    photos: [
+      sc1,
+      sc2,
+      sc3
+    ]
+  },
+  {
+    id: 1,
+    title: "Wedding Ceremony",
+    photos: [
+      sc1,
+      sc2,
+      sc3
+    ]
+  },
+  {
+    id: 1,
+    title: "Wedding Ceremony",
+    photos: [
+      sc1,
+      sc2,
+      sc3
+    ]
+  },
+  {
+    id: 1,
+    title: "Wedding Ceremony",
+    photos: [
+      sc1,
+      sc2,
+      sc3
+    ]
+  },
+  {
+    id: 1,
+    title: "Wedding Ceremony",
+    photos: [
+      sc1,
+      sc2,
+      sc3
+    ]
+  },
+
+  {
+    id: 1,
+    title: "Wedding Ceremony",
+    photos: [
+      sc1,
+      sc2,
+      sc3
+    ]
+  },
+  {
+    id: 1,
+    title: "Wedding Ceremony",
+    photos: [
+      sc1,
+      sc2,
+      sc3
+    ]
+  },
+  {
+    id: 1,
+    title: "Wedding Ceremony",
+    photos: [
+      sc1,
+      sc2,
+      sc3
+    ]
+  },
+  {
+    id: 1,
+    title: "Wedding Ceremony",
+    photos: [
+      sc1,
+      sc2,
+      sc3
+    ]
+  },
+  {
+    id: 1,
+    title: "Wedding Ceremony",
+    photos: [
+      sc1,
+      sc2,
+      sc3
+    ]
+  },
+  // jitna chahe utna add kr liyo 
+  // aur thoda jldi kr liyo
+];
+
 const Gallery = () => {
   return (
-    <div className="coming">
-      <img src={coming} alt="" />
+    <div className="gallery-container">
+      {events.map((event) => (
+        <div key={event.id} className="gevent-card">
+          <h2 className="event-title">{event.title}</h2>
+          <div className="event-photos">
+            {event.photos.map((photo, index) => (
+              <img key={index} src={photo} alt={`Event ${event.id} - Photo ${index + 1}`} className="event-photo" />
+            ))}
+          </div>
+          <button>See More</button>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default Gallery;
