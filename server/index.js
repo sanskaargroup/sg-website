0519485472d1ab3c76ddc0a1f08c70c34ff38ee0
Tanay Kumar <tanaykmrdev@gmail.com>
2024-09-07 22:22:01 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ dotenv.config({path: "../.env"});
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 app.use(express.static("public"));
 app.use("/*", (req, res) => {
