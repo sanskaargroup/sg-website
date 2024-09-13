e34e0bcbcbd4101cb369b2810b03d77e465197f8
Tanay Kumar <tanaykmrdev@gmail.com>
2024-09-13 10:53:08 +0530

    rebuild with a responsive gallery

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,7 +6,8 @@ import {enquiryFormSchema} from "../utils/schema";
 import "./ContactForm.css";
 
 const port = import.meta.env.VITE_PORT;
-const BASE_URL = process.env.VITE_BASE_URL || "";
+// const BASE_URL = process.env.VITE_BASE_URL || "";
+const BASE_URL = import.meta.env.VITE_BASE_URL || "";
 
 const ContactForm = () => {
 	const [formData, setFormData] = useState({
